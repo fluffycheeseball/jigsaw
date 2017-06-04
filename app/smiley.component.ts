dc2c0a9b1f05685dfb531342cc2675dc0fd9e090
fluffycheeseball <billvolestate@gmail.com>
2017-06-04 12:37:44 +0100

    temp dnd

--- app/smiley.component.ts
+++ app/smiley.component.ts
@@ -1,4 +1,4 @@
-import { Component, Directive, ElementRef, AfterViewInit, Input, Renderer, ContentChild, ViewChildren, ViewChild  } from '@angular/core';
+import { Component, Directive, ElementRef, AfterViewInit, Input, Renderer, ContentChild, ViewChildren, ViewChild ,HostListener } from '@angular/core';
 
 @Component({
     selector: 'smiley',
@@ -7,38 +7,68 @@ import { Component, Directive, ElementRef, AfterViewInit, Input, Renderer, Conte
 export class SmileyDirective {
     @ViewChild('canvas') canvasRef:ElementRef;
     private canvas: any;
+    private left: number;
+    private top: number;
+    private dragon: boolean;
     @Input('size') size: number;
     @Input('color') color: string;
 
     constructor(private el: ElementRef, private renderer: Renderer) {
-        // console.log("SmileyDirective constructor: size: ", this.size, ", color: ", this.color, ', this.canvas: ', this.canvas);
+         console.log("SmileyDirective constructor: size: ", this.size, ", color: ", this.color, ', this.canvas: ', this.canvas);
     }
 
+     @HostListener('mousedown', ['$event'])
+     mouseDown(event: MouseEvent) {
+         this.dragon = true;
+    //      var mousePos = this.getMousePos( event);
+ //   this.draw(mousePos.x,mousePos.y)
+  } 
+
+       @HostListener('mouseup') mouseUp(event: MouseEvent) {
+           this.dragon = false;
+  //  console.log("mouse up");
+  } 
+
+       @HostListener('mousemove', ['$event']) 
+       mouseMove(event: MouseEvent) {
+           if(this.dragon) {
+          var mousePos = this.getMousePos( event);
+    this.draw(mousePos.x,mousePos.y)
+           }
+  } 
+
     ngOnInit() {
-        // console.log("SmileyDirective ngOnInit: size: ", this.size, ", color: ", this.color, ', this.canvas: ', this.canvas);
+         console.log("SmileyDirective ngOnInit: size: ", this.size, ", color: ", this.color, ', this.canvas: ', this.canvas);
     }
 
     ngAfterViewInit() {
-        // console.log("SmileyDirective ngAfterViewInit: size: ", this.size, ", color: ", this.color, ', this.canvas: ', this.canvas);
+         console.log("SmileyDirective ngAfterViewInit: size: ", this.size, ", color: ", this.color, ', this.canvas: ', this.canvas);
         this.canvas = this.canvasRef.nativeElement;
         this.canvas.width = 5000;
         this.canvas.height = 4000;
-        this.draw();
+        this.draw(0,0);
     }
 
-    draw() {
+    getMousePos( evt) {
+        var rect = this.canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+    } 
+
+    draw(left: number, top:number) {
         if (this.canvas.getContext) {
             let canvas = this.canvas;
             if (canvas.getContext){
                 var ctx = canvas.getContext('2d');
-      var imageObj = new Image();
+        var imageObj = new Image();
 
-      imageObj.onload = function() {
-        ctx.drawImage(imageObj, 69, 50);
-      };
-      imageObj.src = '../../biggrin.png';
-  
-            }
+        imageObj.onload = function() {
+            ctx.drawImage(imageObj, left, top);
+        };
+        imageObj.src = '../../biggrin.png';
+        }
         }
     }
 }
